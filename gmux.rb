0ac0859a26366a4db6b3aa650f6e3aaf61bce41b
Vincent Cheung <vinche7@gmail.com>
2016-12-03 13:36:20 -0800

    gmux: delete command / bash completion

--- gmux.rb
+++ gmux.rb
@@ -1,13 +1,15 @@
 class Gmux < Formula
   desc "Tmux Session Management written in Golang"
   homepage "https://github.com/davinche/gmux"
-  url "https://github.com/davinche/gmux/releases/download/v0.0.1/gmux-0.0.1.tar.gz"
-  sha256 "c3a457ccb78acf268edab1cbc13168a8b143e75984d620c6bf6cdd3564523333"
+  url "https://github.com/davinche/gmux/releases/download/v0.0.2/gmux-0.0.2.tar.gz"
+  sha256 "da90c03633dae5ebddb591c0c3b1083292f9b7b1a8035ebbe17eaa32db12c1f0"
 
   bottle :unneeded
 
   def install
     bin.install "gmux"
+    prefix.install "autocomplete"
+    prefix.install "install_completion.sh"
   end
 
   test do
