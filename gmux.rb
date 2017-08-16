6e419a7800900acfbc521ee01df9cc69828bf921
Vincent Cheung <vinche7@gmail.com>
2017-08-16 12:00:14 -0700

    gmux: v0.1.4

--- gmux.rb
+++ gmux.rb
@@ -1,8 +1,8 @@
 class Gmux < Formula
   desc "Tmux Session Management written in Golang"
   homepage "https://github.com/davinche/gmux"
-  url "https://github.com/davinche/gmux/releases/download/v0.1.3/gmux-0.1.3.tar.gz"
-  sha256 "5b33c0af87cb90d83e4b5dafec90db1ecffe592c13dfd28dbb7cf0a282aa6c6b"
+  url "https://github.com/davinche/gmux/releases/download/v0.1.4/gmux-0.1.4.tar.gz"
+  sha256 "e6ab46eefcdedcb9c651f52b901d2129992ea25c981a4383c21f0bffedadeab6"
 
   bottle :unneeded
 
